32929c6482af644d96412e496ded8e3b37a139b8
Kizito Njoku <nkizito57@gmail.com>
2019-10-15 17:28:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,9 +6,9 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  puts array.length
+  find_min_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+  find_min_value(array)
 end
