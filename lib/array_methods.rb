cc67fc6538833df33a5acde5020cfb3a54f74461
Kizito Njoku <nkizito57@gmail.com>
2019-10-15 17:31:03 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,14 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  while value_to_find < array {
+    array[value_to_find]
+    value_to_find++
+  }
 end
 
 def find_max_value(array)
-  # Add your solution here
+  find_max_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+  find_min_value(array)
 end
